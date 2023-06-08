387000a7342d28577403ffbf2f17e63dec04b598
yannemal <56878180+Yannemal@users.noreply.github.com>
2023-06-09 00:23:41 +0800

    Initial commit

---
+++ WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by yann guyt on 09/06/2023.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
