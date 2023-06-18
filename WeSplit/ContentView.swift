d3559760b4ebf65e5703eb901b6b034855a4b7b5
yannemal <56878180+Yannemal@users.noreply.github.com>
2023-06-18 15:23:05 +0800

    emoticon update
    Project 1 update: finished Day 16-18 100DaysOfSwiftUI
    We Split app can divide the bill amount + add tip:
    - enter bill amount in a TextField in local currency
    - use a digit with decimal keyboard
    - Select the number of friends using a Picker view.
    - Select a pre set Tip amount using a segmented
       Picker View

--- WeSplit/ContentView.swift
+++ WeSplit/ContentView.swift
@@ -1,26 +1,86 @@
 //
 //  ContentView.swift
 //  WeSplit
-//
+//  Day17
 //  Created by yannemal on 09/06/2023.
 //
 
 import SwiftUI
 
 struct ContentView: View {
+    // data goes here w default values incl
+    @State private var checkAmount = 0.0
+    @State private var numberOfPeople = 2
+    @State private var tipPercentage = 20
+    // private var currencySymbol = Locale.current.currencySymbol
+    @FocusState private var amountIsFocused : Bool
+    //constant
+    let tipPercentages = [5, 7, 10, 12, 15, 20, 25, 0]
+    
+    var totalPerPerson: Double {
+        // the Picker range starts counting at 2 index2 == 4 therefore
+        let peopleCountCorrect = Double(numberOfPeople + 2)
+        let tipSelection = Double(tipPercentage)
+        
+        let tipValue = checkAmount / 100 * tipSelection
+        let grandTotal = checkAmount + tipValue
+        let amountPerPerson = grandTotal / peopleCountCorrect
+        return amountPerPerson
+    }
+    
+    // viewDidLoad in SwiftUI
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        NavigationStack {
+            Form {
+                Section{
+                    // Text("add bill total:")
+                    TextField("Amount", value: $checkAmount, format: .currency(code: Locale.current.currency?.identifier ?? "USD"))
+                        .keyboardType(.decimalPad)
+                        .focused($amountIsFocused) // read write to the focus true false
+                    
+                    Picker("Number of People", selection: $numberOfPeople) {
+                        ForEach(2..<100) {
+                            Text(" \($0) people")
+                        }
+                    }
+                } header: {
+                    Text("add 💰 amount and  👯‍♀️people paying:")
+                }
+                
+                Section{
+                    Picker("Tip percentage", selection: $tipPercentage) {
+                        ForEach(tipPercentages, id: \.self) {
+                            Text($0, format: .percent)
+                        }
+                    }
+                    .pickerStyle(.segmented)
+                } header: {
+                    Text("How much 👍🏼👍🏼  would you like to leave ?")
+                }
+                
+                
+                Section{
+                    Text("Amount evenly split:")
+                    Text(totalPerPerson, format: .currency(code: Locale.current.currency?.identifier ?? "USD"))
+                }
+            }
+            // modify the form
+            .navigationTitle("We Split 🍕 🍔 🍺")
+            .toolbar {
+                ToolbarItemGroup(placement: .keyboard) {
+                    Spacer() // pushes "Done" all the way to the right
+                    Button("⬇️") {
+                        amountIsFocused = false
+                    }
+                }
+            }
         }
-        .padding()
+    }
+    struct ContentView_Previews: PreviewProvider {
+        static var previews: some View {
+            ContentView()
+        }
     }
 }
 
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
-    }
-}
+/* weSplit app : type in the amount of the check and how many people at the table. Set a tipPErcentage. The app will calculate how much each pays when evenly split. */
